4850d010d42699b6667e3cc84fe96218f09d8ef7
hltt <3094289517@qq.com>
2025-01-14 18:12:41 +0800

    修复了飞机被击败后还能发射子弹的bug

--- scripts/plane/plane.gd
+++ scripts/plane/plane.gd
@@ -21,7 +21,7 @@ func _physics_process(delta: float) -> void:
 
 #功能函数
 func fire() -> void: #开火
-	if Input.is_action_pressed("fire") and is_ok_to_fire:
+	if not is_game_over and Input.is_action_pressed("fire") and is_ok_to_fire:
 		var bullet_node = bullet_scene.instantiate() 
 		bullet_node.position=position+Vector2(0,-11)
 		get_tree().current_scene.add_child(bullet_node)
