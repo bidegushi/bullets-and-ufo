9f9f7170032356a9ef7e8a6645d844fac609e9b9
hltt <3094289517@qq.com>
2025-01-14 14:11:02 +0800

    删了一个缩进

--- scripts/enemy/skeleton.gd
+++ scripts/enemy/skeleton.gd
@@ -32,15 +32,15 @@ func _physics_process(delta: float) -> void:
 
 
 func _on_area_entered(area: Area2D) -> void:
-		if not is_dead and area.is_in_group("player_bullet"): #如果碰到了玩家的子弹
-			sprite.visible=false #消失
-			Global.create_explosion_with_255_rgb(position, 177, 233, 169 ,1) #播放爆炸效果
-			is_dead=true #调整变量
-			area.queue_free() #删除子弹
-			get_tree().current_scene.score+=get_tree().current_scene.skeleton_score#玩家加分
-			
-			await get_tree().create_timer(0.5).timeout #0.5秒后删除 同时爆炸效果也没了
-			queue_free()
+	if not is_dead and area.is_in_group("player_bullet"): #如果碰到了玩家的子弹
+		sprite.visible=false #消失
+		Global.create_explosion_with_255_rgb(position, 177, 233, 169 ,1) #播放爆炸效果
+		is_dead=true #调整变量
+		area.queue_free() #删除子弹
+		get_tree().current_scene.score+=get_tree().current_scene.skeleton_score#玩家加分
+		
+		await get_tree().create_timer(0.5).timeout #0.5秒后删除 同时爆炸效果也没了
+		queue_free()
 
 
 func _on_fire_timer_timeout() -> void:
