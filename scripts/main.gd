14d58f77e5aeac82a4cd3089d155c15cd5a41e9f
hltt <3094289517@qq.com>
2025-01-14 18:35:50 +0800

    使用继承实现了黄金飞机一次发射3个并排同方向子弹

--- scripts/main.gd
+++ scripts/main.gd
@@ -11,7 +11,8 @@ extends Node2D
 var is_game_over : bool = false #游戏是否结束
 
 var plane_node 
-var plane_scene=load("res://scene/plane/plane.tscn")
+#var plane_scene=load("res://scene/plane/plane.tscn")
+var plane_scene=load("res://scene/plane/vip_plane.tscn")
 
 var score : int = 0 #游戏得分
 
--- scripts/plane/vip_plane.gd
+++ scripts/plane/vip_plane.gd
@@ -5,10 +5,18 @@ extends "res://scripts/plane/plane.gd"
 
 
 
+
 func fire() -> void :
 	if Input.is_action_pressed("fire") and is_ok_to_fire:
-		var bullet_node = bullet_scene.instantiate() 
-		bullet_node.position=position+Vector2(0,-11)
-		get_tree().current_scene.add_child(bullet_node)
+		
+		var bullet_distance = 10 #三个子弹之间的间隔
+		
+		for i in range(3):
+			var bullet_node = bullet_scene.instantiate() 
+			bullet_node.position=position+Vector2(-bullet_distance+bullet_distance*i,-11)
+			get_tree().current_scene.add_child(bullet_node)
+		
+		
+		
 		is_ok_to_fire=false
 		fire_Timer.start()
